197c40ef28e6c4c04204f3233e81892c18a6f084
Shashank Kumar Srivastava <shashanksrivastav9800@gmail.com>
2023-12-07 21:47:49 +0530

    commit all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/1.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "..........";
 
 /**
  * Project list
@@ -31,9 +31,9 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "https://github.com/shashank0412/Analysis-of-sample-superstore-",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+      "Analysis of Sample Superstore",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
